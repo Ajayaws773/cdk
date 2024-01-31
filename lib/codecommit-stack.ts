fae79799d0645d7c45e6140864aef8da9f7b48ea
ajay <ajay@gmail.com>
2024-01-31 23:17:23 +0530

    first commit

--- lib/codecommit-stack.ts
+++ lib/codecommit-stack.ts
@@ -1,10 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 
 export class CodecommitStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const repository = new codecommit.Repository(this, 'MyRepo', { repositoryName: 'test' });
 
     // The code that defines your stack goes here
 
