2e2bba87e1a6b45a1470c4b2e0b60dd53cbe81ac
ajay <ajay@gmail.com>
2024-02-01 22:00:23 +0530

    codebuild

--- bin/codecommit.ts
+++ bin/codecommit.ts
@@ -2,20 +2,11 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CodecommitStack } from '../lib/codecommit-stack';
+//import { Codecommit2Stack } from '../lib/codecommit2stack';
+//import { CodecommitStacknew } from '../lib/codecommit-stack';
 
 const app = new cdk.App();
-new CodecommitStack(app, 'CodecommitStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+new CodecommitStack(app, 'CodecommitStack');
+//new Codecommit2Stack(app, 'CodecommitStack2');
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+//new CodecommitStacknew(app, 'CodecommitStacknew');
--- lib/codecommit-stack.ts
+++ lib/codecommit-stack.ts
@@ -2,17 +2,26 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+
 
 export class CodecommitStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const repository = new codecommit.Repository(this, 'MyRepo', { repositoryName: 'test' });
+    const repository = new codecommit.Repository(this, 'MyRepo', { repositoryName: 'newtest' });
+   new codebuild.Project(this, 'MyFirstCodeCommitProject', {
+  source: codebuild.Source.codeCommit({ repository }),
+});
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CodecommitQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
+
+//export class CodecommitStacknew extends cdk.Stack {
+//  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+//    super(scope, id, props);
+ ////   const repository = new codecommit.Repository(this, 'MyRepo', { repositoryName: 'newtest' });
+  //  const repository = new codecommit.Repository(this, 'MyRepo', { repositoryName: 'test2' });
+    
+//  }
+//}
